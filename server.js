780831af5c969b1d687f2aa016a393e9e1046d69
tillzmh <tillzmh@users.noreply.github.com>
2022-10-08 11:01:05 -0400

    created apps variables

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.require('.routes/index'));
+
