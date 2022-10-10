053c39922e9a7f125cf351f0319b94cbcf1a5600
tillzmh <tillzmh@users.noreply.github.com>
2022-10-10 18:38:11 -0400

    created index months

--- utilies/dateFormat.js
+++ utilies/dateFormat.js
@@ -1,5 +1,7 @@
 // const e = require("express");
 
+const { models } = require("mongoose");
+
 const addDateSuffix = date => {
     let dateString = date.toString();
 
@@ -19,3 +21,22 @@ const addDateSuffix = date => {
     return dateString;
 };
 
+module.exports =(timestamp) => {
+    let months;
+
+    {months = {
+        0: 'Jan',
+        1: 'Feb',
+        2: 'Mar',
+        3: 'Apr',
+        4: 'May',
+        5: 'Jun',
+        6: 'Jul',
+        7: 'Aug',
+        8: 'Sep',
+        9: 'Oct',
+        10: 'Nov',
+        11: 'Dec',
+    };
+}
+}
