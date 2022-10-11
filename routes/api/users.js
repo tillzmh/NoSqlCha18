249f7ec11729275e3cbd12c610bb100a23cb8f88
tillzmh <tillzmh@users.noreply.github.com>
2022-10-11 16:46:04 -0400

    user routes created

---
+++ routes/api/users.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend,
+} = require('../../controllers/users-const');
+
+router.route('/') //set up get all and post routes at api/users
+    .get(getAllUsers)
+    .post(createUser)
+
+router.route('/:id') //set up get, put and delete but _id
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser)
+
+    router.route('/:userID/friends/:friendID') // post and delete friends 
+    .post(addFriend)
+    .delete(deleteFriend)
+
+    module.exports = router;
