224ff31c003801456eea74ef291259e60e3e5803
tillzmh <tillzmh@users.noreply.github.com>
2022-10-11 14:58:17 -0400

    thoughts routes created

--- routes/api/thoughts.js
+++ routes/api/thoughts.js
@@ -1,9 +1,30 @@
 const router = require('express').Router();
-// const {
-//     getAllThoughts;
-//     getThoughtsById;
-//     addThoughts;
-//     deleteThoughts;
-//     addReactions;
-//     deleteReactions;
-// } 
+const {
+    getAllThoughts,
+    getThoughtById,
+    addThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+} = require('../../controllers/thoughts-cont');
+
+router.route('/') //set up get all thoughts at api/thoughts
+.get(getAllThoughts)
+
+router.route('/:userId') //set up post at api/thoughts/:userId
+.post(addThought)
+
+router.route('/userId/:thoughtId') // set up get, put and delete at api/thoughts/:userId/:thoughtId
+.get(getThoughtById)
+.put(updateThought)
+.delete(deleteThought)
+
+router.route('/userId/:thoughtId/reactions/:reactionID') //set up post reactionat api/userId/:thoughtId/reactions/:reactionID
+.delete(deleteReaction)
+
+router.route('/userId/:thoughtId/reactions/') //set up pst reaction at /api/thoughts/:userId/:thoughtId/reactions/:reactionId
+.put(addReaction)
+
+
+module.exports = router;
