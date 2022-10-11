32988b3be2dce10eb3ff554dbe3e5a10573ae7f6
tillzmh <tillzmh@users.noreply.github.com>
2022-10-10 21:31:47 -0400

    schema db created

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,2 +1,34 @@
 const {Schema, Types} = require("mongoose")
-const dateFormate = require('../utilies/dateFormat')
+const dateFormat = require('../utilies/dateFormat')
+
+const reactionSchema = new Schema({
+    reactionID:{
+        type:Schema.Types.ObjectedId, 
+        default: () => new Types.ObjectedId(),
+    },
+    reactionBody: {
+        type:String,
+        required:true,
+        maxLenght: 280
+    },
+
+    username: {
+        type: String,
+        required:true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    }
+},
+{
+    toJSON:{
+        getters:true,
+    },
+    id:false,
+    _id:false,
+}
+);
+
+module.exports = reactionSchema;
